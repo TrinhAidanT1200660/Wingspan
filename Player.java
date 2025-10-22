1862081cea47233263d5473e60b7686213b066bf
basilwey <onowey.art@gmail.com>
2025-10-22 14:31:46 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,15 @@
+public class Player
+{
+    private int points;
+    private TreeMap<String, ArrayList<Bird>> board;
+    private TreeMap<String, ArrayList<Integer>> eggs;
+    private ArrayList<Bonus> bonus;
+    private ArrayList<Bird> birdHand;
+    private HashMap<String, Integer> food;
+    private int tucked;
+    private ArrayList<Integer> goalRankings;
+
+    public Player(int points, TreeMap<String, ArrayList<Bird>> board, TreeMap<String, ArrayList<Integer>> eggs, ArrayList<Bonus> bonus, ArrayList<Bird> birdHand, HashMap<String, Integer> food, int tucked, int actionCubes)
+    {}
+
+}
