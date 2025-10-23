402f6b59432653ae6f4e2be0655bdf45b7ccf239
basilwey <onowey.art@gmail.com>
2025-10-22 20:08:10 -0500

    Update Bonus.java

--- Bonus.java
+++ Bonus.java
@@ -1,1 +1,14 @@
+public enum Bonus
+{
+    //VARIABLES:
 
+    //stores the method for the action, retrieved from the abstract method
+    private final BonusInterface bonusAbility;
+
+    //CONSTRUCTOR:
+    private Bonus(BonusInterface bonusAbility)
+    {
+        this.bonusAbility = bonusAbility;
+    }
+
+    
