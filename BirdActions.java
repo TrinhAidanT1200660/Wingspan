092f14f6e3419d66265c73335ebc8698b1a81b23
TrinhAidanT1200660 <T1200660@students.katyisd.org>
2025-10-22 19:46:29 -0500

    Update BirdActions.java
    Basic structure of birdActions. Will need lots of additions to the function logic and ENUM declarations.

--- BirdActions.java
+++ BirdActions.java
@@ -1,1 +1,24 @@
 
+public enum BirdActions implements BirdActionInterface
+{
+	GRABFISH((player) ->
+	{
+		// this temporary to just show the structure
+	});
+	
+	// stores the action value of each birdAction within the ENUM
+	private final BirdActionInterface action;
+		
+	// constructor that assigns the variable action to the necessary logic
+	private BirdActions(BirdActionInterface action)
+	{
+		this.action = action;
+	}
+	
+	// overriding function for BirdActionInterface; just initiates the action of the bird
+	@Override
+	public void execute(Player player)
+	{
+		action.execute(player);
+	}
+}
