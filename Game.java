397c612e262b899c14d5797dafb542252ec2b4ee
TrinhAidanT1200660 <T1200660@students.katyisd.org>
2025-10-22 20:32:15 -0500

    Update Game.java
    Super basic framework of game. Needs to hold the logic for the flow of the game but can be implemented later.

--- Game.java
+++ Game.java
@@ -1,1 +1,11 @@
+import java.util.*;
 
+public class Game {
+	private int startingActionCubes; // holds the beginning of the rounds starting action cubes; starts at 8 beginning of game
+	private int playerTurn; // holds which player's turn it current is; starts at 1 beginning of game
+	private int startingPlayerTurn; // holds the player who had their turn first; starts at 1 beginning of game
+	private boolean gameMode; // holds the gamemode being played: true for competitive, false for non-competitive
+	private int roundsPlayed; // holds the number of rounds played so far; starts at 0 beginning of game
+	
+	// NOTHING MADE YET
+}
