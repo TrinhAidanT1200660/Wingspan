ff8705c68124e0e695d164694edd4d01b22f852d
aidantrinh <T1200660@students.katyisd.org>
2025-10-25 22:15:04 -0500

    Changed Bird, BirdAction, BirdActionInterface, BirdInstance, Game, Player
    Bird: changed the form of image loading to make it lazy loading to same RAM. Also added deckCount decreaser and updated performAction to include gameContext.
    BirdAction: added gameContext to the methods and functions and finished ALLDRAW1BIRD function.
    BirdActionInterface: updated it to add gameContext
    BirdInstance: updated it to add gameContext
    Game: Added playerlist arrayLIst and return method for it
    Player: Changed birdHand and corresponding methods and constructor to keep as Bird. Plan to instantiate bird to BirdInstance when placed on board.

--- Game.java
+++ Game.java
@@ -6,6 +6,9 @@ public class Game {
 	private int startingPlayerTurn; // holds the player who had their turn first; starts at 1 beginning of game
 	private boolean gameMode; // holds the gamemode being played: true for competitive, false for non-competitive
 	private int roundsPlayed; // holds the number of rounds played so far; starts at 0 beginning of game
-	
-	// NOTHING MADE YET
+	private ArrayList<Player> playerList;
+
+	// RETURN METHODS
+
+	public ArrayList<Player> getPlayers() { return playerList; }
 }
