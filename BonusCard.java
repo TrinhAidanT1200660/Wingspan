d877c39418c1d479e3c9b6a0ffb93ddca87af876
aidantrinh <T1200660@students.katyisd.org>
2025-10-25 22:39:17 -0500

    BonusCard ENUM Changed
    Changed image loading to lazy image loading to save RAM usage

--- BonusCard.java
+++ BonusCard.java
@@ -6,7 +6,6 @@ public enum BonusCard implements BonusCardInterface
     // temporary bonus card just so the errors stop. also an example
     ECOLOGIST("temp.jpg", (player) -> {
         // Some logic that finds the habitats with the least amount of birds
-        // test change in code
     });
 
     //VARIABLES:
@@ -14,28 +13,44 @@ public enum BonusCard implements BonusCardInterface
     //stores the method for the action, retrieved from the abstract method
     private final BonusCardInterface bonusAbility;
     private int deckCount;
-    private BufferedImage bonusImage;
+    private final String imageFileString;
+	private BufferedImage bufferedImageFile;
 
     //CONSTRUCTOR:
-    private BonusCard(String imageFile, BonusCardInterface bonusAbility)
+    private BonusCard(String imageFileString, BonusCardInterface bonusAbility)
     {
         this.bonusAbility = bonusAbility;
         this.deckCount = 1;
-
-        // directly uses the image file name to create buffered image
-		try
-		{
-			// need to define the package that will be used to hold images before this is done
-			this.bonusImage = ImageIO.read(BonusCard.class.getResource("/packageNameHere/" + imageFile));
-		}
-		catch (Exception E)
-		{
-			System.out.println("Could not load bonus ENUM image: " + E.getMessage());
-			return;
-		}
+        this.imageFileString = imageFileString;
     }
 
-    //VOID METHODS:
+    //RETURN METHODS:
+    // returns the deckCount of bonusCard
+    public int getDeckCount() { return deckCount; }
+
+    // returns a BufferedImage with the bonus' image file
+    public BufferedImage getImage() 
+	{ 
+		if (bufferedImageFile == null)
+		{
+			// directly uses the image file name to create buffered image
+			try
+			{
+				// need to define the package that will be used to hold images before this is done
+				this.bufferedImageFile = ImageIO.read(BonusCard.class.getResource("/packageNameHere/" + imageFileString));
+			}
+			catch (Exception E)
+			{
+				System.out.println("Could not load bonus ENUM image: " + E.getMessage());
+				return null;
+			}
+		}
+
+		return bufferedImageFile;
+	}
+
+    // VOID METHODS / MUTATOR METHODS:
+    public void removeCardFromDeck() { deckCount -= 1; }
 
     //carries out the card's scoring
     @Override
@@ -44,12 +59,5 @@ public enum BonusCard implements BonusCardInterface
         bonusAbility.bonusScore(player);
     }
 
-    //RETURN METHODS:
-    public int getDeckCount() { return deckCount; }
-
-    // returns a BufferedImage with the bird's image file
-	public BufferedImage getImage() { return bonusImage; }
-
-    //MUTATOR METHODS:
-    public void removeCardFromDeck() { deckCount -= 1; }
+    public void releaseImage() { bufferedImageFile = null; }
 }
