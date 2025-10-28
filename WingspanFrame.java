c87059918e441fe9c074b43f1bec60dd49b3695a
AlsouriMohammedK1828812 <k1828812@students.katyisd.org>
2025-10-27 23:07:43 -0500

    Created Favicon
    Set application icon from the penguin to Wingspan

--- WingspanFrame.java
+++ WingspanFrame.java
@@ -1,8 +1,16 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class WingspanFrame extends JFrame {
     public WingspanFrame(String framename) {
         super(framename);
+        try {
+            Image icon = new ImageIcon(getClass().getResource("/resources/images/wingspan_favicon.png")).getImage();
+            setIconImage(icon);
+        } catch (NullPointerException e) {
+            System.err.println("Icon image not found");
+        }
         setSize(1500, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(new WingspanPanel());
