863b4d4d2239b2d74c17a2505a8993eda7a52e3f
aidantrinh <T1200660@students.katyisd.org>
2025-10-28 20:48:00 -0500

    quick push for live share

--- Game.java
+++ Game.java
@@ -6,7 +6,7 @@ public class Game {
 	private int startingPlayerTurn; // holds the player who had their turn first; starts at 1 beginning of game
 	private boolean gameMode; // holds the gamemode being played: true for competitive, false for non-competitive
 	private int roundsPlayed; // holds the number of rounds played so far; starts at 0 beginning of game
-	private ArrayList<Player> playerList;
+	private ArrayList<Player> playerList; // holds the list of players in the game
 
 	// CONSTRUCTOR
 	public Game()
@@ -19,7 +19,15 @@ public class Game {
 		for(int i = 0; i < 5; ++i) { playerList.add(new Player()); }
 	}
 
+	// GAME METHODS
+
+	
+
 	// RETURN METHODS
 
 	public ArrayList<Player> getPlayers() { return playerList; }
+
+	// MUTATOR METHODS
+
+	public void setGameMode(Boolean gameMode) { this.gameMode = gameMode; }
 }
